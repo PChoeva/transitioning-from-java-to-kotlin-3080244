34f4d11b431f4a749441b281053c03bf78b126b2
Petya Choeva <petya.choeva@gmail.com>
2024-01-10 19:19:08 +0000

    Added kotlin serialization for json

---
+++ ktor-store-demo/src/main/kotlin/com/linkedinlearning/plugins/ContentNegotiation.kt
@@ -0,0 +1,12 @@
+package com.linkedinlearning.plugins
+
+import io.ktor.server.application.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun Application.configureContentNegotiation() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+}
